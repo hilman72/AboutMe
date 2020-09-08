2271cd3f18a32e766965154023bee653d02d99fe
hilman72 <hilman91112@gmail.com>
2020-09-08 11:05:31 +0800

    fixing

--- scripts/index.js
+++ scripts/index.js
@@ -33,77 +33,143 @@ function click_fun() {
 /**************************************************** */
 
 // sidebar
-$(document).ready(function(){
-  $("#sidebarCollapse,.sidebar-header,#side1,#side2,#side3,#side4").on('click',function(){
-    $("#sidebar, #content").toggleClass('active');
-  })
-})
+$(document).ready(function () {
+  $("#sidebarCollapse,.sidebar-header,#side1,#side2,#side3,#side4").on(
+    "click",
+    function () {
+      $("#sidebar, #content").toggleClass("active");
+    }
+  );
+});
 
 /**************************************************** */
 
 // my detail
 
-
-$('.myDetail-item').click(function () {
-  $('.collapse').collapse('hide');
+$(".myDetail-item").click(function () {
+  $(".collapse").collapse("hide");
 });
 
-// coding skills
-$(".myDetail-coding-skill").on('click',function(){
-  $("#bar-html,#bar-css,#bar-js,#bar-bs,#bar-jq").animate({
-    width: "10%"
-  }, 500);
-})
+$(".myDetail-myProject").hover(function () {
+  $(".fa-project-diagram").addClass("animate__bounce");
+});
+$(".myDetail-myProject").bind(
+  "webkitAnimationEnd oAnimationEnd MSAnimationEnd",
+  function () {
+    $(".fa-project-diagram").removeClass("animate__bounce");
+  }
+);
 
-$(".myDetail-other-skill,.myDetail-resume,.myDetail-myProject").on('click',function(){
-  $("#bar-html,#bar-css,#bar-js,#bar-bs,#bar-jq").animate({
-    width: "0%"
-  }, 100);
-})
+$(".myDetail-coding-skill").hover(function () {
+  $(".fa-tools").addClass("animate__bounce");
+});
+$(".myDetail-coding-skill").bind(
+  "webkitAnimationEnd oAnimationEnd MSAnimationEnd",
+  function () {
+    $(".fa-tools").removeClass("animate__bounce");
+  }
+);
+
+$(".myDetail-other-skill").hover(function () {
+  $(".fa-toolbox").addClass("animate__bounce");
+});
+$(".myDetail-other-skill").bind(
+  "webkitAnimationEnd oAnimationEnd MSAnimationEnd",
+  function () {
+    $(".fa-toolbox").removeClass("animate__bounce");
+  }
+);
+
+$(".myDetail-resume").hover(function () {
+  $(".fa-file").addClass("animate__bounce");
+});
+$(".myDetail-resume").bind(
+  "webkitAnimationEnd oAnimationEnd MSAnimationEnd",
+  function () {
+    $(".fa-file").removeClass("animate__bounce");
+  }
+);
+// coding skills
+$(".myDetail-coding-skill").on("click", function () {
+  $("#bar-html,#bar-css,#bar-js,#bar-bs,#bar-jq").animate(
+    {
+      width: "10%",
+    },
+    500
+  );
+});
+
+$(".myDetail-other-skill,.myDetail-resume,.myDetail-myProject").on(
+  "click",
+  function () {
+    $("#bar-html,#bar-css,#bar-js,#bar-bs,#bar-jq").animate(
+      {
+        width: "0%",
+      },
+      100
+    );
+  }
+);
 
 // other skills
 
-$(".myDetail-other-skill").on('click',function(){
-  $("#bar-ch").animate({
-    width: "100%"
-  }, 500);
-})
+$(".myDetail-other-skill").on("click", function () {
+  $("#bar-ch").animate(
+    {
+      width: "100%",
+    },
+    500
+  );
+});
 
-$(".myDetail-other-skill").on('click',function(){
-  $("#bar-eng").animate({
-    width: "80%"
-  }, 500);
-})
+$(".myDetail-other-skill").on("click", function () {
+  $("#bar-eng").animate(
+    {
+      width: "80%",
+    },
+    500
+  );
+});
 
-$(".myDetail-other-skill").on('click',function(){
-  $("#bar-jap").animate({
-    width: "90%"
-  }, 500);
-})
+$(".myDetail-other-skill").on("click", function () {
+  $("#bar-jap").animate(
+    {
+      width: "90%",
+    },
+    500
+  );
+});
 
-$(".myDetail-other-skill").on('click',function(){
-  $("#bar-mand").animate({
-    width: "50%"
-  }, 500);
-})
+$(".myDetail-other-skill").on("click", function () {
+  $("#bar-mand").animate(
+    {
+      width: "50%",
+    },
+    500
+  );
+});
 
-$(".myDetail-coding-skill,.myDetail-resume,.myDetail-myProject").on('click',function(){
-  $("#bar-ch,#bar-eng,#bar-jap,#bar-mand").animate({
-    width: "0%"
-  }, 100);
-})
-
+$(".myDetail-coding-skill,.myDetail-resume,.myDetail-myProject").on(
+  "click",
+  function () {
+    $("#bar-ch,#bar-eng,#bar-jap,#bar-mand").animate(
+      {
+        width: "0%",
+      },
+      100
+    );
+  }
+);
 
 /**************************************************** */
 
 // slideshow
 
-
-var swiper = new Swiper('.swiper-container', {
-  effect: 'coverflow',
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   coverflowEffect: {
     rotate: 50,
     stretch: 0,
@@ -113,20 +179,10 @@ var swiper = new Swiper('.swiper-container', {
   },
   loop: true,
   pagination: {
-    el: '.swiper-pagination',
-
+    el: ".swiper-pagination",
   },
   navigation: {
-  nextEl: '.swiper-button-next',
-  prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
-
-
-
-
-
-
-
-
-
